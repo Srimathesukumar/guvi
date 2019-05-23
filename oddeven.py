9df982453d282678a29deb0c9ec0f6fc872e8f28
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-05-23 02:53:37 -0700

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -3,7 +3,7 @@ number=int(input())
 if(number>0):
   if(number%2==0):
     print('Even')
-  elif(int(number%2==1):
-      print('Odd')
+  elif(number%2==1):
+    print('Odd')
 else:
-  print('invalid')
+  print('Invalid')
