14a3632279dba500d81425709afe9d7d707b02ca
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-05-26 07:44:04 -0700

    Create large.py

---
+++ large.py
@@ -0,0 +1,13 @@
+# guvi
+''' Largest of three number '''
+number = input()
+number = number.split()
+num1 = int(number[0])
+num2 = int(number[1])
+num3 =int(number[2]) 
+if(num1>num2 and num1>num3):
+  print(num1)
+elif(num2>num1 and num2>num3):
+  print(num2)
+else:
+  print(num3)
