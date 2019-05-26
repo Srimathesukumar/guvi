992944c0b0383bef8bf45a90e68a9e72d75b1aa5
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-05-26 07:50:55 -0700

    Update large.py

--- large.py
+++ large.py
@@ -9,5 +9,13 @@ if(num1>num2 and num1>num3):
   print(num1)
 elif(num2>num1 and num2>num3):
   print(num2)
+elif(num3>num1 and num3>num2):
+  print(num3)
+elif(num1==num2 and num2==num3):
+  print(num1)
+elif(num1==num2):
+  print(num1)
+elif(num2==num3):
+  print(num2)
 else:
   print(num3)
