58dcb908a61175b11fd7e0bd077cc7d101b167ac
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-05-31 20:34:20 -0700

    Update max.py

--- max.py
+++ max.py
@@ -1,6 +1,8 @@
 # guvi
 ''' Maximum number among 10 numbers '''
-value = input()
-a=value.split()
-a.sort()
-print(a[-1])
+value=list(map(int,input().split()))
+max=value[0]
+for i in value:
+  if(max<i):
+    max=i 
+print(max)
