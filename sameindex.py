498ecd659481ce992696cdb6c2f807b5365ad200
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-06-03 07:29:07 -0700

    Create sameindex.py

---
+++ sameindex.py
@@ -0,0 +1,13 @@
+# guvi
+''' Print the number equal to their index value in sorted order '''
+n = int(input())
+a = list(map(int,input().split()))
+b = []
+for i in range(0,n):
+  if(a[i]==i):
+    b.append(i)
+c=set(b)
+if(len(c)==0):
+  print('-1')
+else:
+  print(*c)
