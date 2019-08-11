f0ee05c14b3ed37bd21b99cc72b7473e821be199
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-08-11 02:56:47 -0700

    Create points.py

---
+++ points.py
@@ -0,0 +1,13 @@
+# guvi
+''' 3 points lie in the same line '''
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
+if(a==c==e):
+  print('yes')
+elif(b==d==f):
+  print('yes')
+elif(a==b and c==d and e==f):
+  print('yes')
+else:
+  print('no')
