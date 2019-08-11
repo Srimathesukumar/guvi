4149db417042bd9048b232c26b65f30696a87f92
Srimathesukumar <44056934+Srimathesukumar@users.noreply.github.com>
2019-08-11 02:43:42 -0700

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,11 +1,20 @@
 # guvi
 ''' Encoded string by adding 3 in each character '''
 n=input()
-a=list(n)
-l=['A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-b=[]
-for i in range(0,len(a)):
-  for j in range(0,len(l)):
-    if n[i] in l[j]:
-      b.append(l[j+3])
-print("".join(b))
+b=''
+for i in n:
+    if i=="X":
+        b+="A"
+    elif i=="x":
+        b+="a"
+    elif i=="Y":
+        b+="B"
+    elif i=="y":
+         b+="b"
+    elif i=="Z":
+        b+="C"
+    elif i=="z":
+        b+="c"
+    else:
+        b+=chr(ord(i)+3)
+print(b)
